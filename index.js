9946b1d92463577aa1dbee27e83c78b5db1b39c6
ben-asa-cloudflare <asa@ben.engineer>
2026-03-14 22:17:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const path = url.pathname;
-
     const email = request.headers.get("cf-access-authenticated-user-email") || "Unknown User";
     const country = request.headers.get("cf-ipcountry") || "XX";
     const timestamp = new Date().toUTCString();
-
     if ((path.startsWith("/insecure/") || path.startsWith("/secure/")) && path.split("/")[2]) {
       const targetCountry = path.split("/")[2].toLowerCase();
       const fileName = `${targetCountry}.png`;
-
       const object = await env.FLAGS_BUCKET.get(fileName);
-
       if (object === null) {
         return new Response("Flag asset not found in R2", { status: 404 });
       }
-
       const headers = new Headers();
       object.writeHttpMetadata(headers);
       headers.set("Content-Type", "image/png"); // Appropriate content type
       return new Response(object.body, { headers });
     }
-
     if (path === "/insecure" || path === "/insecure/") {
       const htmlContent = `
         <!DOCTYPE html>
@@ -34,8 +28,11 @@ export default {
         </body>
         </html>
       `;
-
-      if (path === "/secure" || path === "/secure/") {
+      return new Response(htmlContent, {
+        headers: { "Content-Type": "text/html" },
+      });
+    }
+    if (path === "/secure" || path === "/secure/") {
       const htmlContent = `
         <!DOCTYPE html>
         <html>
@@ -46,12 +43,9 @@ export default {
         </body>
         </html>
       `;
-
-      
       return new Response(htmlContent, {
         headers: { "Content-Type": "text/html" },
       });
-    }
-    
-  }
+    }     
+  },
 };
